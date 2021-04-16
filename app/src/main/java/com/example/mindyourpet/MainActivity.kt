bc49e97d05390fb835edbc40a2fcdc63f3ee5038
iserrano <iserrano@mills.edu>
2021-04-16 15:02:45 -0700

    WIP

--- app/src/main/java/com/example/mindyourpet/MainActivity.kt
+++ app/src/main/java/com/example/mindyourpet/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.mindyourpet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
