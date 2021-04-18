09b8dff5f332155ed0d2df51a4f0e729585b1a2b
iserrano <iserrano@mills.edu>
2021-04-18 12:54:17 -0700

    Create menu.xml

--- app/src/main/java/com/example/mindyourpet/MainActivity.kt
+++ app/src/main/java/com/example/mindyourpet/MainActivity.kt
@@ -2,15 +2,29 @@ package com.example.mindyourpet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import android.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
-
-    //private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // Note that the Toolbar defined in the layout has the id "my_toolbar"
-        setSupportActionBar(findViewById(R.id.toolbar))
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.getItemId()
+        if (id == R.id.edit_button) {
+            Toast.makeText(this, "item clicked", Toast.LENGTH_LONG).show()
         }
+        return super.onOptionsItemSelected(item)
+    }
 }
