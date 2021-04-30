dd104084fc8d7031aa9b42b922ce889b0580f013
iserrano <iserrano@mills.edu>
2021-04-29 19:29:24 -0700

    Refactor AllPetsList code and related layouts

--- app/src/main/java/com/example/mindyourpet/PetAdapter.kt
+++ app/src/main/java/com/example/mindyourpet/PetAdapter.kt
@@ -3,6 +3,7 @@ package com.example.mindyourpet
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -17,17 +18,26 @@ class PetAdapter : RecyclerView.Adapter<PetAdapter.ViewHolder>() {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val pet = data[position]
-        val res = holder.itemView.context.resources
-        holder.name.text = pet.name
+        holder.bind(pet)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.pet_item_view, parent, false)
-        return ViewHolder(view)
+        return ViewHolder.from(parent)
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val name: TextView = itemView.findViewById(R.id.pet_name)
+    class ViewHolder private constructor (itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val name: Button = itemView.findViewById(R.id.pet_name)
+
+        fun bind(item: Pet) {
+            name.text = item.name
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflator = LayoutInflater.from(parent.context)
+                val view = layoutInflator.inflate(R.layout.pet_item_view, parent, false)
+                return ViewHolder(view)
+            }
+        }
     }
 }
